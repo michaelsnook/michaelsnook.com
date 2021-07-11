43d1f023c63da517f64d5f70e0a31d4af20a168a
Michael Snook <me@michaelsnook.com>
2021-07-11 17:59:53 +0530

    Fix dateTime property

--- components/DateSpan.js
+++ components/DateSpan.js
@@ -2,13 +2,13 @@ export default function DateSpan({ dateText }) {
   const date = new Date(dateText)
   const output = date.toLocaleString(
     'en-IN',
-    { 
-      weekday: 'long', 
-      year: 'numeric', 
-      month: 'long', 
+    {
+      weekday: 'long',
+      year: 'numeric',
+      month: 'long',
       day: 'numeric',
     },
   )
-  
-  return <time datetime={dateText}>{output}</time>    
+
+  return <time dateTime={dateText}>{output}</time>
 }
