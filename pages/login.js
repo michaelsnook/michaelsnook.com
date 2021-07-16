0620b4bc48001dacd700ebc0348958448cb90d7d
Michael Snook <me@michaelsnook.com>
2021-07-16 17:44:53 +0530

    Add login page and form component

---
+++ pages/login.js
@@ -0,0 +1,14 @@
+import Layout from '../components/Layout'
+import LoginForm from '../components/LoginForm'
+
+// import { useContext } from 'react'
+// const user = useContext(UserContext)
+
+export default function Login() {
+
+  return (
+    <Layout>
+      <LoginForm />
+    </Layout>
+  )
+}
