364f1259c79fdccb9964543eae1ae6eee9b8b037
Michael Snook <me@michaelsnook.com>
2021-08-05 14:28:34 +0530

    Unescape <>s in markdown
    This commit also refactors the PrintMarkdown component
    noting that processSync().result already returns a React
    component and so no wrapping is required. This means we
    don't lose readability by returning result directly, so
    no additional variable assignment is required.
    
    This component will rerender hundreds or thousands of times
    while composing a post so the performance/memory matter.

--- components/PrintMarkdown.js
+++ components/PrintMarkdown.js
@@ -1,16 +1,12 @@
-import unified from 'unified';
-import parse from 'remark-parse';
-import remark2react from 'remark-react';
+import unified from 'unified'
+import parse from 'remark-parse'
+import remark2react from 'remark-react'
 
 export default function PrintMarkdown({ markdown }) {
-  const content = unified()
+  return unified()
     .use(parse)
     .use(remark2react)
-    .processSync(markdown).result;
-
-  return (
-    <>
-      {content}
-    </>
-  );
+    .processSync(
+      markdown?.replace('&lt;', '<').replace('&gt;', '>')
+    ).result
 }
