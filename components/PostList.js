68f5c85e507ffda070927c71e43c8201ca654dd7
Michael Snook <me@michaelsnook.com>
2021-08-06 12:57:14 +0530

    Clean up PostList; shorter, clearer

--- components/PostList.js
+++ components/PostList.js
@@ -1,27 +1,21 @@
 export default function PostList({ posts }) {
-  return (
-    <>
-      {posts.length === 0 ? (
-        <p>loading posts...</p>
-      ) : (
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 place-content-stretch">
-          {posts.map(post => (
-            <a key={`post-${post.id}`} className="border rounded" href={`/posts/${post.id}`}>
-              {post.image &&
-                <img className="rounded-t" src={post.image} alt='' />
-              }
-              <p className="text-2xl font-display p-4 text-cyan-700 hover:underline">
-                {post.title}
-              </p>
-              {!post.image &&
-                <p className="p-4">
-                  {post.excerpt}
-                </p>
-              }
-            </a>
-          ))}
-        </div>
-      )}
-    </>
+  return posts.length === 0 ? <p>loading posts...</p> : (
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 place-content-stretch">
+      {posts.map(post => (
+        <a key={`post-${post.id}`} className="border rounded" href={`/posts/${post.id}`}>
+          {post.image &&
+            <img className="rounded-t" src={post.image} alt="" />
+          }
+          <p className="text-2xl font-display p-4 text-cyan-700 hover:underline">
+            {post.title}
+          </p>
+          {!post.image &&
+            <p className="p-4">
+              {post.excerpt}
+            </p>
+          }
+        </a>
+      ))}
+    </div>
   )
 }
