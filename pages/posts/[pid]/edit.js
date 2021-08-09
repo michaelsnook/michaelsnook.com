f948cda8d95f610ca12278a0ba0a43831a3a41d0
Michael Snook <me@michaelsnook.com>
2021-08-09 19:07:22 +0530

    Prettier checkbox (heroicon svg)

--- pages/posts/[pid]/edit.js
+++ pages/posts/[pid]/edit.js
@@ -60,7 +60,7 @@ export default function EditPost() {
               <InputImage register={register} error={errors.image} />
 
               <div className="flex justify-between">
-                <span>
+                <span className="flex">
                   <button
                     type="submit"
                     className="button solid"
@@ -72,7 +72,11 @@ export default function EditPost() {
                       : 'Save edits'
                     }
                   </button>
-                  {isSubmitSuccessful && !isDirty && <span className="mx-2">☑️</span>}
+                  {isSubmitSuccessful && !isDirty && (
+                    <svg xmlns="http://www.w3.org/2000/svg" className="ml-2 h-5 w-5 place-self-center text-green-600" viewBox="0 0 20 20" fill="currentColor">
+                      <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd" />
+                    </svg>
+                  )}
                 </span>
                 <Link href={`/posts/${pid}`}>
                   <a className="button outline">
