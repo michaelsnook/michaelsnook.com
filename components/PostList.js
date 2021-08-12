1832ba43d44b58d0a311592fa3ffaf97ec48dd7f
Michael Snook <me@michaelsnook.com>
2021-08-12 22:29:39 +0530

    Use Link component in PostList

--- components/PostList.js
+++ components/PostList.js
@@ -1,23 +1,26 @@
 import Image from 'next/image'
+import Link from 'next/link'
 
 const PostCard = ({ id, image, title, excerpt }) => (
-  <a role="listitem" className="border rounded" href={`/posts/${id}`}>
-    {image && (
-      <div className="relative min-h-64 sm:min-h-40">
-        <Image
-          className="rounded-t"
-          src={image}
-          alt=""
-          layout="fill"
-          objectFit="cover"
-        />
-      </div>
-    )}
-    <p className="text-2xl font-display p-4 text-cyan-700 hover:underline">
-      {title}
-    </p>
-    {!image && <p className="p-4">{excerpt}</p>}
-  </a>
+  <Link href={`/posts/${id}`}>
+    <a role="listitem" className="border rounded">
+      {image && (
+        <div className="relative min-h-64 sm:min-h-40">
+          <Image
+            className="rounded-t"
+            src={image}
+            alt=""
+            layout="fill"
+            objectFit="cover"
+          />
+        </div>
+      )}
+      <p className="text-2xl font-display p-4 text-cyan-700 hover:underline">
+        {title}
+      </p>
+      {!image && <p className="p-4">{excerpt}</p>}
+    </a>
+  </Link>
 )
 
 export default function PostList({ posts }) {
