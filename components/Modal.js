b6fbc1dd8da0467d028acebe799bfef952a64c4f
Michael Snook <me@michaelsnook.com>
2021-08-26 15:20:10 +0530

    Refactor Modal, fix removeEventListener, render null not hidden

--- components/Modal.js
+++ components/Modal.js
@@ -1,5 +1,28 @@
 import { useState, useEffect, useCallback } from 'react'
 
+const CloseButton = ({ close }) => (
+  <button
+    onClick={close}
+    className="py-1 px-2 absolute top-2 right-3 text-xs text-gray-500 flex place-items-center"
+  >
+    close&nbsp;
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      className="h-4 w-4"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth={2}
+        d="M6 18L18 6M6 6l12 12"
+      />
+    </svg>
+  </button>
+)
+
 export default function Modal({ showing, children }) {
   const [isShowing, setIsShowing] = useState(showing || false)
 
@@ -10,47 +33,24 @@ export default function Modal({ showing, children }) {
   }, [])
 
   useEffect(() => {
-    document.addEventListener('keydown', escFunction, false)
-    return () => {
-      document.removeEventListener('keydown', escFunction, false)
-    }
-  }, [])
+    if (isShowing) document.addEventListener('keydown', escFunction, false)
+    else document.removeEventListener('keydown', escFunction, false)
+  }, [isShowing, escFunction])
 
-  return (
+  return isShowing ? (
     <div
       onClick={event => {
         if (event.target === event.currentTarget) setIsShowing(false)
       }}
-      className={`z-40 bg-black bg-opacity-50 px-2 pt-10 ${
-        isShowing ? 'fixed' : 'hidden'
-      } top-0 left-0 right-0 bottom-0`}
+      className="z-40 bg-black bg-opacity-50 px-2 pt-10 fixed top-0 left-0 right-0 bottom-0"
     >
       <div
         role="dialog"
         className="bg-white rounded max-w-xl mx-auto px-6 py-4 relative"
       >
         {children}
-        <button
-          onClick={() => setIsShowing(false)}
-          className="py-1 px-2 absolute top-2 right-3 text-xs text-gray-500 flex place-items-center"
-        >
-          close&nbsp;
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-4 w-4"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={2}
-              d="M6 18L18 6M6 6l12 12"
-            />
-          </svg>
-        </button>
+        <CloseButton close={() => setIsShowing(false)} />
       </div>
     </div>
-  )
+  ) : null
 }
