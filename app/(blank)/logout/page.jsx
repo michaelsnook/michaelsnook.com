f0a5e6240d5f71961cca19e99801036a166d52a8
Michael Snook <michael.snook@gmail.com>
2024-05-06 20:30:02 +0530

    Move login/logout to app router

---
+++ app/(blank)/logout/page.jsx
@@ -0,0 +1,36 @@
+'use client'
+
+import { useEffect, useState } from 'react'
+import Link from 'next/link'
+import { AlertBox } from '../../../components/lib'
+import { postLogout } from '../../../lib/auth'
+
+export default function Logout() {
+  const [isFinished, setIsFinished] = useState()
+  useEffect(() => {
+    postLogout().then(() => setIsFinished(true))
+  }, [])
+
+  return (
+    <main className="single-col">
+      {!isFinished ? (
+        <h1 className="h5">Logging out...</h1>
+      ) : (
+        <AlertBox>
+          <h1 className="h3">Logged out</h1>
+          <p className="my-4">
+            Congratulations, you may now{' '}
+            <Link href="/" className="link">
+              return to the home page
+            </Link>{' '}
+            or{' '}
+            <Link href="/login" className="link">
+              log in again
+            </Link>
+            .
+          </p>
+        </AlertBox>
+      )}
+    </main>
+  )
+}
