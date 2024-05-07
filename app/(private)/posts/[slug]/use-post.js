4340dfbb7d08c243fa0d16f6bed133ae180081ea
Michael Snook <michael.snook@gmail.com>
2024-05-08 04:52:16 +0530

    Move the edit page into the app router as well

---
+++ app/(private)/posts/[slug]/use-post.js
@@ -0,0 +1,19 @@
+import useSWR from 'swr'
+import { fetchOnePost } from '@/lib/posts'
+
+export default function usePost(slug, reset) {
+  const { data, error, isLoading } = useSWR(slug, fetchOnePost, {
+    onSuccess: post => reset(post),
+    revalidateOnFocus: false,
+    revalidateOnReconnect: false,
+    refreshWhenOffline: false,
+    refreshWhenHidden: false,
+    refreshInterval: 0,
+  })
+
+  return {
+    post: data,
+    loadError: error,
+    isLoading,
+  }
+}
