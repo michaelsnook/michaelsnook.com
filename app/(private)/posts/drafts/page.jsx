f3e1b2167c46639fea9a7b0627b1bb6b305bda5d
Michael Snook <michael.snook@gmail.com>
2024-05-08 00:44:37 +0530

    Add draft posts screen to app router

---
+++ app/(private)/posts/drafts/page.jsx
@@ -0,0 +1,32 @@
+'use client'
+
+import Link from 'next/link'
+import useSWR from 'swr'
+import PostList from '../../../../components/PostList'
+import Banner from '../../../../components/Banner'
+import { ErrorList } from '../../../../components/lib'
+import { fetchDraftPosts } from '../../../../lib/posts'
+
+export default function Drafts() {
+  const { data, error } = useSWR(`posts/drafts`, fetchDraftPosts)
+
+  return (
+    <>
+      <Banner
+        title="Your draft posts"
+        description="Consider finishing one or two, maybe 🤷"
+        small
+      />
+      <main className="container py-5">
+        <div className="flex flex-row justify-between items-center">
+          <h2 className="h2">Draft posts</h2>
+          <Link href="/posts/new" className="button outlines">
+            New post
+          </Link>
+        </div>
+        <ErrorList summary="Can't load drafts" error={error} />
+        <PostList posts={data} />
+      </main>
+    </>
+  )
+}
