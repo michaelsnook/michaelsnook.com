c75267baf254ba9d6b2483b0c6c65d9fcd32acfe
Michael Snook <michael.snook@gmail.com>
2024-05-10 18:23:04 +0530

    Make the logout page look nicer

--- app/logout/page.jsx
+++ app/logout/page.jsx
@@ -19,24 +19,26 @@ export default function Page() {
 
   return (
     <main className="single-col">
-      {!isFinished ? (
-        <h1 className="h5">Logging out...</h1>
-      ) : (
-        <AlertBox>
-          <h1 className="h3">Logged out</h1>
-          <p className="my-4">
-            Congratulations, you may now{' '}
-            <Link href="/" className="link">
-              return to the home page
-            </Link>{' '}
-            or{' '}
-            <Link href="/login" className="link">
-              log in again
-            </Link>
-            .
-          </p>
-        </AlertBox>
-      )}
+      <AlertBox type={isFinished ? 'neato' : 'info'}>
+        {!isFinished ? (
+          <h1 className="h5">Logging out...</h1>
+        ) : (
+          <>
+            <h1 className="h3">Logged out</h1>
+            <p className="my-4">
+              Congratulations, you may now{' '}
+              <Link href="/" className="link">
+                return to the home page
+              </Link>{' '}
+              or{' '}
+              <Link href="/login" className="link">
+                log in again
+              </Link>
+              .
+            </p>
+          </>
+        )}
+      </AlertBox>
     </main>
   )
 }
--- components/lib/AlertBox.js
+++ components/lib/AlertBox.js
@@ -2,6 +2,10 @@ const AlertBox = ({ type, children }) => {
   const classes =
     type === 'success'
       ? 'border-green-400 text-green-800 bg-green-200'
+      : type === 'info'
+      ? 'border-blue-400 text-blue-800 bg-blue-200'
+      : type === 'neato'
+      ? 'border-pink-400 text-pink-800 bg-pink-200'
       : 'text-gray-600'
   return <div className={`border rounded p-10 py-6 ${classes}`}>{children}</div>
 }
