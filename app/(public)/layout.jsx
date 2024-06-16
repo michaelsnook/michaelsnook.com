d95ca193fd5d9566bc50b3b0f09bdd82589aff21
Em Snook <me@michaelsnook.com>
2024-06-16 18:25:59 +0530

    Replace 404.js with not-found.js for the app router

--- app/(public)/layout.jsx
+++ app/(public)/layout.jsx
@@ -4,27 +4,33 @@ export default function Layout({ children }) {
   return (
     <>
       {children}
-      <footer className="border-t w-full py-10 mt-10">
-        <nav className="space-x-4 space-y-10 py-4 mx-auto text-center">
-          <Link href="/" className="text-cyan-content underline">
-            home
-          </Link>
-          <a
-            className="text-cyan-content underline"
-            href="https://twitter.com/michaelsnook"
-          >
-            twitter
-          </a>
-          <a
-            className="text-cyan-content underline"
-            href="https://github.com/michaelsnook"
-          >
-            github
-          </a>
-          <span>CC0 1.0</span>
-          <span>&lt;/&gt; by me</span>
-        </nav>
-      </footer>
+      <Footer />
     </>
+  )
+}
+
+export function Footer() {
+  return (
+    <footer className="border-t w-full py-10 mt-10">
+      <nav className="space-x-4 space-y-10 py-4 mx-auto text-center">
+        <Link href="/" className="text-cyan-content underline">
+          home
+        </Link>
+        <a
+          className="text-cyan-content underline"
+          href="https://twitter.com/michaelsnook"
+        >
+          twitter
+        </a>
+        <a
+          className="text-cyan-content underline"
+          href="https://github.com/michaelsnook"
+        >
+          github
+        </a>
+        <span>CC0 1.0</span>
+        <span>&lt;/&gt; by me</span>
+      </nav>
+    </footer>
   )
 }
