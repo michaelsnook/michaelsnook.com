9f8254584e06da99f9a39a140579f65f228f3d04
em snook <michael.snook@gmail.com>
2024-11-09 13:57:12 +0530

    Gen supabase types and type the API client

--- app/supabase-client.ts
+++ app/supabase-client.ts
@@ -1,6 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
+import type { Database } from 'types/supabase'
 
-const supabase = createClient(
+const supabase = createClient<Database>(
 	process.env.NEXT_PUBLIC_SUPABASE_API_URL,
 	process.env.NEXT_PUBLIC_SUPABASE_ANON_TOKEN,
 )
